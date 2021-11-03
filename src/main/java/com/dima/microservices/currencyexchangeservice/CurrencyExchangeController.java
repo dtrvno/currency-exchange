ad3c5894b6b65d84c6919f1fafd30e5a4e9bb570
dtrvno <dimatru@gmail.com>
2021-11-03 14:35:01 -0700

    added zipkin

--- src/main/java/com/dima/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/dima/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.dima.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private CurrencyExchangeRepository repository;
     @Autowired
@@ -18,6 +21,7 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to) {
         CurrencyExchange currencyExchange=repository.findByFromAndTo(from,to);
+        logger.info("Retrieve exchange called with parameters from {} to {}",from,to);
         if (currencyExchange==null)
             throw new RuntimeException("Unable to find data for " + from + " to " +" to ");
         String port=environment.getProperty("local.server.port");
